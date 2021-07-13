2e95bf87f533f0e2a639078d2236b6891163d7da
Jake Thrasher <mjakethrasher@gmail.com>
2021-07-12 20:00:19 -0700

    router

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,20 @@
 import LandingPage from './components/landing/LandingPage';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  } from 'react-router-dom';
 function App() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <Router>
+      <Switch>
+      <Route exact path='/'>
+        <LandingPage/>
+      </Route>
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
