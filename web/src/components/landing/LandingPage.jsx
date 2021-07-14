8b85b4da3fce20697eec58b2b399137570523265
Jake Thrasher <mjakethrasher@gmail.com>
2021-07-13 21:07:56 -0700

    update landing/about

--- web/src/components/landing/LandingPage.jsx
+++ web/src/components/landing/LandingPage.jsx
@@ -5,6 +5,7 @@ import './landing.css'
 import Projects from '../projects/Projects';
 import ContactForm from '../contact/ContactForm';
 import { useScroll } from '../../hooks/useScroll';
+import { BsArrowDownShort } from 'react-icons/bs'
 
 export default function LandingPage() {
   const {
@@ -23,6 +24,23 @@ export default function LandingPage() {
         handleAboutScroll={handleAboutScroll}
         handleContactScroll={handleContactScroll}
       />
+       <section
+        
+        className='landingContainer' >
+        <div className='centerContent'>
+          <h3 className='greeting'>Hello! I'm</h3>
+          <h1 className='name'>Jake<br></br>Thrasher.</h1>
+          <h2 className='statement'>
+            I'm a full-stack software engineer in Portland, Oregon. I like to build apps that are modern and inspired. I'm all about tackling complex problems and learning as much as I can along the way.
+          </h2>
+        </div>
+        <div className='chevronDiv'>
+          <BsArrowDownShort
+            onClick={handleAboutScroll}
+            size='4rem' 
+            style={{color:'rgba(85, 85, 85, 0.646)'}}/>
+        </div>
+      </section>
       <About ref={aboutRef}/>
       <Projects ref={portfolioRef}/>
       <ContactForm ref={contactRef}/>
--- web/src/components/tech/TechStack.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TechStack = () => {
-  return(
-    <p>hey</p>
-  );
-};
-export default TechStack
